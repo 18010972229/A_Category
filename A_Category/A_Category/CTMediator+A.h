dff5ec564b0e3d8e9af5f6d701b917a371df5129
chenfj <446311697@qq.com>
2017-01-08 21:12:15 +0800

    添加分类文件
    添加分类文件

---
+++ A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by 陈福杰 on 2017/1/8.
+//  Copyright © 2017年 com.xxx. All rights reserved.
+//
+
+#import <CTMediator/CTMediator.h>
+
+@interface CTMediator (A)
+
+@end
---
+++ A_Category/A_Category/CTMediator+A.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+A.m
+//  A_Category
+//
+//  Created by 陈福杰 on 2017/1/8.
+//  Copyright © 2017年 com.xxx. All rights reserved.
+//
+
+#import "CTMediator+A.h"
+
+@implementation CTMediator (A)
+
+@end
